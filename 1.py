48bcbd69d7019a614d82b89c3362cf876c4a76c7
VendoLD <imalukataki@gmail.com>
2025-02-18 18:59:09 +0300

    1

--- 1.py
+++ 1.py
@@ -2,31 +2,83 @@ import keyboard
 import os
 import time
 
-map_size = list(map(int,(input('Enter map size in format: x, y; Input "0" for default map').split(', '))))
+map_size = list(map(int, input('Enter map size in format: x, y; Input "0" for default map').split(', ')))
 
-if map_size == 0:
-    MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', 'P', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+        ['#', 'P', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
         ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
-else:
-    MAP = []
-    for i in range(map_size[1]):
-        MAP.append([])
-        for j in range(map_size[0]):
-            if i == 0 or i == (map_size[1] - 1) or j == 0 or j == map_size[0] - 1:
-                MAP[i].append('#')
-            else:
-                MAP[i].append('.')
 
 
-CHARACTER_INDEX_X = 4
-CHARACTER_INDEX_Y = 2
+# if map_size[0] == 0:
+#     MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+#         ['#', 'P', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+#         ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
+# else:
+#     MAP = []
+#     for i in range(map_size[1]):
+#         MAP.append([])
+#         for j in range(map_size[0]):
+#             if i == 0 or i == (map_size[1] - 1) or j == 0 or j == map_size[0] - 1:
+#                 MAP[i].append('#')
+#             else:
+#                 MAP[i].append('.')
+
+
+INTERFACE = [['-', '-', '-', '-', '-']]
+
+
+
+
+class Character:
+    def __init__(self, start_x, start_y, map):
+        self.character_x = start_x
+        self.character_y = start_y
+        self.map = map
+
+
+    def move(self, x_move, y_move):
+
+        new_x = self.character_x + x_move
+        new_y = self.character_y + y_move
+
+        if self.map[new_y][new_x] == '#':
+            return
+        
+        self.map[self.character_y][self.character_x], self.map[new_y][new_x] = self.map[new_y][new_x], self.map[self.character_y][self.character_x]
+
+        self.character_x = new_x
+        self.character_y = new_y
+    
+    def move_up(self):
+        self.move(0, -1)
+
+
+    def move_left(self):
+        self.move(-1, 0)
+
+
+    def move_down(self):
+        self.move(0, 1)
+
+
+    def move_right(self):
+        self.move(1, 0)
+
+
+
+
 
 
 def render(map):
@@ -35,57 +87,25 @@ def render(map):
             print(char, end=' ')
         print()
 
-    # os.system('cls')
 
 
-def move_up(map, x, y):
-    if map[y - 1][x] == '#':
-        return
-    map[y][x], map[y - 1][x] = map[y - 1][x], map[y][x]
-    y -= 1
-    global CHARACTER_INDEX_Y
-    CHARACTER_INDEX_Y = y
 
 
-def move_left(map, x, y):
-    if map[y][x - 1] == '#':
-        return
-    map[y][x], map[y][x - 1] = map[y][x - 1], map[y][x]
-    x -= 1
-    global CHARACTER_INDEX_X
-    CHARACTER_INDEX_X = x
-
-
-def move_down(map, x, y):
-    if map[y + 1][x] == '#':
-        return
-    map[y][x], map[y + 1][x] = map[y + 1][x], map[y][x]
-    y += 1 
-    global CHARACTER_INDEX_Y
-    CHARACTER_INDEX_Y = y
-
-
-def move_right(map, x, y):
-    if map[y][x + 1] == '#':
-        return
-    map[y][x], map[y][x + 1] = map[y][x + 1], map[y][x]
-    x += 1
-    global CHARACTER_INDEX_X
-    CHARACTER_INDEX_X = x
+character = Character(1, 1, MAP)
 
 
 def on_w_click():
-    move_up(MAP, CHARACTER_INDEX_X, CHARACTER_INDEX_Y)
+    character.move_up()
 
 def on_a_click():
-    move_left(MAP, CHARACTER_INDEX_X, CHARACTER_INDEX_Y)
+    character.move_left()
 
 def on_s_click():
-    move_down(MAP, CHARACTER_INDEX_X, CHARACTER_INDEX_Y)
+    character.move_down()
 
 
 def on_d_click():
-    move_right(MAP, CHARACTER_INDEX_X, CHARACTER_INDEX_Y)
+    character.move_right()
 
 
 keyboard.add_hotkey('w', on_w_click)
