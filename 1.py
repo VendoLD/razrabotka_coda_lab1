9314a456c29330028b2f1804f95e4820b65245c6
VendoLD <imalukataki@gmail.com>
2025-02-19 20:56:16 +0300

    2

--- 1.py
+++ 1.py
@@ -2,22 +2,10 @@ import keyboard
 import os
 import time
 
-map_size = list(map(int, input('Enter map size in format: x, y; Input "0" for default map').split(', ')))
+map_size = list(map(int, input('Enter map size in format: x, y; Input "0" for default map: ').split(', ')))
 
-MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-        ['#', 'P', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-        ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
-
-
-# if map_size[0] == 0:
-#     MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-#         ['#', 'P', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+# MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+#         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
 #         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
 #         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
 #         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
@@ -25,98 +13,219 @@ MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 #         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
 #         ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
 #         ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
-# else:
-#     MAP = []
-#     for i in range(map_size[1]):
-#         MAP.append([])
-#         for j in range(map_size[0]):
-#             if i == 0 or i == (map_size[1] - 1) or j == 0 or j == map_size[0] - 1:
-#                 MAP[i].append('#')
-#             else:
-#                 MAP[i].append('.')
 
+def map_generation():
+    if map_size[0] == 0:
+        MAP = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'], 
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
+    else:
+        if map_size[0] < 5 or map_size[1] < 5:
+            print('Minimum map size: 5x5')
+            return 
+        MAP = []
+        for i in range(map_size[1] + 1):
+            MAP.append([])
+            for j in range(map_size[0] + 1):
+                if i == 0 or i == map_size[1] or j == 0 or j == map_size[0]:
+                    MAP[i].append('#')
+                else:
+                    MAP[i].append('.')
+    return MAP
 
-INTERFACE = [['-', '-', '-', '-', '-']]
+MAP = map_generation()
+
 
 
 
 
 class Character:
-    def __init__(self, start_x, start_y, map):
-        self.character_x = start_x
-        self.character_y = start_y
-        self.map = map
+    def __init__(self, hp, arm, dmg):
+        self.hp = hp
+        self.arm = arm
+        self.dmg = dmg
+        self.map : Map = None
+        self.character_x = None
+        self.character_y = None
 
-
-    def move(self, x_move, y_move):
-
-        new_x = self.character_x + x_move
-        new_y = self.character_y + y_move
-
-        if self.map[new_y][new_x] == '#':
-            return
-        
-        self.map[self.character_y][self.character_x], self.map[new_y][new_x] = self.map[new_y][new_x], self.map[self.character_y][self.character_x]
-
-        self.character_x = new_x
-        self.character_y = new_y
     
     def move_up(self):
-        self.move(0, -1)
+        self.map.move_character(self, 0, -1)
 
 
     def move_left(self):
-        self.move(-1, 0)
+        self.map.move_character(self, -1, 0)
 
 
     def move_down(self):
-        self.move(0, 1)
+        self.map.move_character(self, 0, 1)
 
 
     def move_right(self):
-        self.move(1, 0)
+        self.map.move_character(self, 1, 0)
+    
+
+    def kill(self):
+        self.map.remove_character(self)
+
+
+
+class Map:
+    def __init__(self, map_list: list[list]):
+        self.map_list = map_list
+        self.player = None
+        self.enemies = []
+    
+    def add_character(self, character: Character, x, y):
+        if self.map_list[y][x] != '.':
+            return 
+        
+        self.map_list[y][x] = character
+
+        if isinstance(character, Player):
+            self.player = character
+        else:
+            self.enemies.append(character)
+        
+        character.map = self
+        character.character_x = x
+        character.character_y = y
+    
+    def remove_character(self, character: Character):
+        x = character.character_x
+        y = character.character_y
+
+        self.map_list[y][x] = '.'
+
+        del character
+
+    def move_character(self, character: Character, x_move, y_move):
+        current_x = character.character_x
+        current_y = character.character_y
+
+        new_x = character.character_x + x_move
+        new_y = character.character_y + y_move
+
+        if self.map_list[new_y][new_x] != '.':
+            return
+        
+        self.map_list[current_y][current_x], self.map_list[new_y][new_x] = self.map_list[new_y][new_x], self.map_list[current_y][current_x]
+
+        character.character_x = new_x
+        character.character_y = new_y
+
+    def enemies_around(self):
+        l = [self.map_list[self.player.character_y + 1][self.player.character_x]]
+        l.append(self.map_list[self.player.character_y - 1][self.player.character_x])
+        l.append(self.map_list[self.player.character_y][self.player.character_x + 1])
+        l.append(self.map_list[self.player.character_y][self.player.character_x - 1])
+        result = []
+        for i in l:
+            if isinstance(i, Enemy):
+                result.append(i)
+        return result
+
+
+    def render(self):
+        for string in self.map_list:
+            for char in string:
+                if isinstance(char, Player):
+                    print('P', end=' ')
+                elif isinstance(char, Enemy):
+                    print('E', end=' ')
+                else:
+                    print(char, end=' ')
+            print()
+
+
+class Enemy(Character):
+    pass
+
+
+
+class Player(Character):
+    def __init__(self, name, hp, arm, dmg):
+        super().__init__(hp, arm, dmg)
+        self.name = name
+
+    def attack(self):
+        enemies_to_attack = self.map.enemies_around()
+        for enemy in enemies_to_attack:
+            enemy.hp -= 100
+            if enemy.hp <= 0:
+                enemy.kill()
 
 
 
 
 
 
-def render(map):
-    for string in map:
-        for char in string:
-            print(char, end=' ')
-        print()
 
 
-
-
-
-character = Character(1, 1, MAP)
 
 
 def on_w_click():
-    character.move_up()
+    player.move_up()
 
 def on_a_click():
-    character.move_left()
+    player.move_left()
 
 def on_s_click():
-    character.move_down()
+    player.move_down()
 
 
 def on_d_click():
-    character.move_right()
+    player.move_right()
+
+def on_space_click():
+    player.attack()
 
 
 keyboard.add_hotkey('w', on_w_click)
 keyboard.add_hotkey('a', on_a_click)
 keyboard.add_hotkey('s', on_s_click)
 keyboard.add_hotkey('d', on_d_click)
+keyboard.add_hotkey('space', on_space_click)
+
+# def render(map):
+#     for string in map:
+#         for char in string:
+#             if isinstance(char, Character):
+#                 print('P', end=' ')
+#             else:
+#                 print(char, end=' ')
+#         print()
 
 
+
+
+player = Player('Player123', 100, 10, 10)
+enemy = Enemy(10, 10, 10)
+enemy2 = Enemy(100, 10, 10)
+map = Map(MAP)
+
+
+map.add_character(player, 4, 3)
+map.add_character(enemy, 6, 3)
+map.add_character(enemy2, 5, 2)
+
+
+INTERFACE = f'''
+--------------------------------------------------------
+NAME {player.name} | HP {player.hp} | ARM {player.arm} | DMG {player.dmg}
+--------------------------------------------------------
+w - up | a - left | s - down | d - right | space - attack
+'''
 
 while True:
-    render(MAP)
+    map.render()
+    print(INTERFACE)
     time.sleep(0.001)
     os.system('cls')
 
